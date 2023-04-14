ddb13f6abbc5065ee92ff5b3c34ed2a63f0f6be2
TristanDDuncan <tristanduncan822@gmail.com>
2023-04-14 14:55:39 -0400

    created book model

--- backend/database/models.py
+++ backend/database/models.py
@@ -31,3 +31,13 @@ class Car(db.Model):
     user = db.relationship("User")
 
 # TODO: Add your models below, remember to add a new migration and upgrade database
+class Review (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.String(255),nullable=False)
+    text = db.Column(db.String(255),nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeginKey('user.id'))
+    
+    user = db.relationship("User")
+
+    
--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -1,6 +1,6 @@
 from flask_marshmallow import Marshmallow
 from marshmallow import post_load, fields
-from database.models import User, Car
+from database.models import User, Car, Review
 
 ma = Marshmallow()
 
@@ -59,3 +59,16 @@ cars_schema = CarSchema(many=True)
 
 
 # TODO: Add your schemas below
+class ReviewSchema(ma.Schema):
+    id = fields.Integer(primary_key=True)
+    book_id = fields.String()
+    text = fields.String()
+    rating = fields.Integer()
+    user_id = fields.Integer()
+    user = ma.Nested(UserSchema, many=False)
+    class Meta:
+        fields = ("id", "book_id", "text", "rating", "user_id", "user")
+
+    @post_load
+    def create_review(self, data, **kwargs):
+        return Review(**data)
